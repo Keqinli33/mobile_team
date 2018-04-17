1866012f385b2d97659b0e010be6bcce699be6bc
keqinli <likeqing0301@gmail.com>
2018-04-16 19:29:53 -0700

    SVM

--- mediate.py
+++ mediate.py
@@ -5,26 +5,49 @@ import feature_core
 import csv
 import os
 
+from sklearn.svm import SVC
+
 # one file
-path = "/Users/keqinli/PycharmProjects/mobile_team/clear_data/open/1.txt"
+
+path2 = "/Users/keqinli/PycharmProjects/mobile_team/clear_data/walk"
+
+x_pathWrite = "/Users/keqinli/PycharmProjects/mobile_team/x.txt"
+y_pathWrite = "/Users/keqinli/PycharmProjects/mobile_team/y.txt"
 #files = os.listdir(path)
 
 def test():
-    lines = list()
-    # with open(path) as file:
-    #     for line in file:
-    #         #line = line.strip()  # or some other preprocessing
-    #         lines.append(np.loadtxt(f, skiprows=1, dtype=int))  # storing everything in memory!
+    path1 = "/Users/keqinli/PycharmProjects/mobile_team/clear_data/open"
+    files1 = os.listdir(path1)
+    results = np.empty((0,19), int)
+    for file in files1:
+        path = path1 + "/" + file
+        lines = np.loadtxt(path, dtype=int)
+        feature_mat1 = lines.reshape(lines.size, 1)
+        result = feature_core.sequence_feature(feature_mat1, 5, 4)
+        results = np.append(results, result, axis=0)
 
-    lines = np.loadtxt(path, dtype=int)
+    X_0 = np.array(results)
+    print(X_0.size)
+    Y_0 = np.zeros(X_0.size)
 
-    narray =   np.array(lines)
-    feature_mat = narray.reshape(narray.size, 1)
-    #a = np.arange(0, 10).reshape((10, 1))
-    print(feature_core.sequence_feature(feature_mat, 5, 4))
+    mat = np.matrix(X_0)
+    with open(x_pathWrite, 'wb') as f:
+        for line in mat:
+            np.savetxt(f, line)
+
+
+    with open(y_pathWrite, 'wb') as f:
+        for 
 
 
 
+    # clf = SVC()
+    # clf.fit(X_0, Y_0)
+
+
+
+    #print(Y_0)
+    #print(Y_0.size)
 
 if __name__ == '__main__':
     test()
